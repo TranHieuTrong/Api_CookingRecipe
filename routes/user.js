5011467c81a6d804afe2740210f6a5564bb94201
TranHieuTrong <zerusboster1689@gmail.com>
2024-06-10 10:36:35 +0700

    HieuTrong

--- routes/user.js
+++ routes/user.js
@@ -16,12 +16,18 @@ router.get("/getUser", async function (req, res, next) {
 // Endpoint để đăng ký người dùng mới
 router.post("/register", async function (req, res, next) {
   try {
-    const { name, email, password } = req.body;
+    const { name, email, password, image, phone } = req.body;
     const checkEmail = await UserModel.findOne({ email });
     if (checkEmail) {
       return res.status(400).json({ message: "Email đã được sử dụng" });
     }
-    const user = await UserController.register(name, email, password);
+    const user = await UserController.register(
+      name,
+      email,
+      password,
+      image,
+      phone
+    );
     res.status(201).json({ message: "Đăng ký thành công", user });
   } catch (error) {
     res.status(401).json({ error: error.message });
@@ -38,6 +44,32 @@ router.post("/login", async function (req, res, next) {
       res.status(200).json({ message: "Đăng nhập thành công", user });
     } else {
       res.status(401).json({ error: "Email hoặc mật khẩu không đúng." });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Endpoint để cập nhật người dùng theo ID
+router.put("/updateUser/:id", async function (req, res, next) {
+  try {
+    const { id } = req.params;
+    const updateData = req.body;
+
+    // Kiểm tra xem email mới có trùng với email của người dùng khác không
+    const existingUser = await UserModel.findOne({
+      email: updateData.email,
+      _id: { $ne: id },
+    });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email đã được sử dụng" });
+    }
+
+    const user = await UserController.updateUser(id, updateData);
+    if (user) {
+      res.status(200).json({ message: "Cập nhật thành công", user });
+    } else {
+      res.status(404).json({ message: "Người dùng không tồn tại" });
     }
   } catch (error) {
     res.status(500).json({ error: error.message });
